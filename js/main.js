6928c067ba3dc0cb3189a995d14460800fc2b622
samuelarellano <sarellano@outlook.com>
2022-02-01 21:44:55 -0800

    Se agregaron las fuentes con fontface

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const $detailsList = document.querySelectorAll('details');
+$detailsList.forEach(($details) => {
+  $details.querySelector('summary').addEventListener('click', expand)
+})
+function expand(){
+  $detailsList.forEach(($details) => {
+    $details.removeAttribute('open')
+  })
+}
